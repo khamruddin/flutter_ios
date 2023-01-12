897aef07714f80324265db67837c3da802a7e24e
khamruddin <74547308+khamruddin@users.noreply.github.com>
2023-01-12 08:45:43 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,22 @@
 
+pipeline{
+  agent {label 'Slave'}
+  
+  stages{
+    
+    stage('code checkout'){
+      steps{
+      git 'https://github.com/khamruddin/sample_flutter_app.git'
+    }
+  }
+    stage('build'){
+      steps{
+       sh '''
+       #!/bin/bash
+       flutter build apk --debug --ignore-deprectaion
+       '''
+      }
+    }
+  
+  }
+}
